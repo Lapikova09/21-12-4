75ca2b3351d20ca20f4edbf94a2c91ae0bb81519
Екатерина <96071466+Lapikova09@users.noreply.github.com>
2023-12-16 17:28:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function clickBtn1(){
+    document.querySelector('.news1').classList.add('removed')
+}
+function clickBtn2(){
+    document.querySelector('.news2').classList.add('removed')
+}
+function clickBtn3(){
+    document.querySelector('.news3').classList.add('removed')
+}
